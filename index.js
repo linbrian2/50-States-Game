0402e1e6b1fe76cce0bb1588dd12b7134c9484ce
Brian Lin <linbrian2@gmail.com>
2020-03-10 17:13:28 -0400

    Removed unnecessary code

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// This is a subset of the states.
-// Use this to actually run the game
-// (assume this is the full set of states.
-// This will make it easier to test.
 var states = ["Idaho", "South Dakota", "Hawaii", "Alaska", "Alabama", "New York"];
 
-// These are all the states.
-// Use this list if you want to test out the full game
+
 var abvMap = {
     "Alabama": "AL",
     "Alaska": "AK",
@@ -59,19 +54,3 @@ var abvMap = {
     "Wisconsin": "WI",
     "Wyoming": "WY",
 }
-
-
-/*
- * The majority of this project is done in JavaScript.
- *
- * 1. Start the timer when the click button is hit. Also, you must worry about
- *    how it will decrement (hint: setTimeout).
- * 2. Check the input text with the group of states that has not already been
- *    entered. Note that this should only work if the game is currently in
- * 3. Realize when the user has entered all of the states, and let him/her know
- *    that he/she has won (also must handle the lose scenario). The timer must be stopped as well.
- *
- * There may be other tasks that must be completed, and everyone's implementation
- * will be different. Make sure you Google! We urge you to post in Piazza if
- * you are stuck.
- */
